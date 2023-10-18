56068dbabaf8da20f9a832b283268688d9e682fc
JordanEckford <jordan.eckford@gmail.com>
2023-10-18 15:22:07 +0100

    fixed problem with undefined being passed in 5 day forecast

--- src/components/location5DayForecast.jsx
+++ src/components/location5DayForecast.jsx
@@ -1,22 +1,21 @@
 import { useState } from "react";
 
 function LocationCardForecast({ weatherForecast }) {
-  if (weatherForecast === undefined) return <p>Loading</p>;
-  const foreCastArr = weatherForecast.list;
-  return (
-    <>
-      {foreCastArr.map((data) => {
-        const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-        return (
-          <ul key={data.dt_txt}>
-            <p>{(data.main.temp - 273.15).toFixed(1)}</p>
-            <p>{data.dt_txt}</p>
-            <img src={iconURL}></img>
-          </ul>
-        );
-      })}
-    </>
-  );
+ const foreCastArr = weatherForecast.list;
+ return (
+  <div className="weather-forecast">
+   {foreCastArr.map((data) => {
+    const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    return (
+     <ul key={data.dt_txt}>
+      <p>{(data.main.temp - 273.15).toFixed(1)}</p>
+      <p>{data.dt_txt}</p>
+      <img src={iconURL}></img>
+     </ul>
+    );
+   })}
+  </div>
+ );
 }
 
 export default LocationCardForecast;
--- src/components/locationDailyForecast.jsx
+++ src/components/locationDailyForecast.jsx
@@ -1,35 +1,32 @@
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import LocationCardForecast from "./location5DayForecast";
 
-function LocationCards({ weather }) {
-  const iconURL = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
-  const test = [weather.coord.lat, weather.coord.lon];
-  return (
-    <>
-      <p>
-        Location: {weather.name}, {weather.sys.country}
-      </p>
-      <p>Location: {weather.weather[0].description}</p>
-      <p>Temperature: {weather.main.temp} </p>
-      <p>
-        Min: {weather.main.temp_min} Max: {weather.main.temp_max}
-      </p>
-      <p>Wind speed: {weather.wind.speed}</p>
-      <img src={iconURL}></img>
-      <LocationCardForecast weather={weather} />
-      <div id="map" height="500px">
-        <MapContainer
-          center={[weather.coord.lat, weather.coord.lon]}
-          zoom={13}
-          scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
-      </div>
-    </>
-  );
+function LocationDailyForecast({ weather }) {
+ console.log("IN DAILY WEATEHR", weather);
+ const iconURL = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+ const test = [weather.coord.lat, weather.coord.lon];
+ return (
+  <div className="daily-weather">
+   <p>
+    Location: {weather.name}, {weather.sys.country}
+   </p>
+   <p>Location: {weather.weather[0].description}</p>
+   <p>Temperature: {weather.main.temp} </p>
+   <p>
+    Min: {weather.main.temp_min} Max: {weather.main.temp_max}
+   </p>
+   <p>Wind speed: {weather.wind.speed}</p>
+   <img src={iconURL}></img>
+   {/* <div id="map" height="500px">
+    <MapContainer center={[weather.coord.lat, weather.coord.lon]} zoom={13} scrollWheelZoom={false}>
+     <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+     />
+    </MapContainer>
+   </div> */}
+  </div>
+ );
 }
 
-export default LocationCards;
+export default LocationDailyForecast;
