84eb75f0d8043ecba383b7aeeaeeb5e5145e8427
JordanEckford <jordan.eckford@gmail.com>
2023-12-04 19:22:18 +0000

    fixed minor issue on med screen sizes where text was overlapping

--- src/components/locationDailyForecast.jsx
+++ src/components/locationDailyForecast.jsx
@@ -12,10 +12,12 @@ export default function LocationDailyForecast({ weather }) {
      {date.slice(8, 10)}-{date.slice(5, 7)}-{date.slice(0, 4)} ----- {date.slice(11, 16)}
     </p>
    </div>
-   <p className="daily-location">
-    {weather.name}, {weather.sys.country}
-   </p>
-   <p className="daily-description">{weather.weather[0].description.slice(0, 1).toUpperCase() + weather.weather[0].description.slice(1)}</p>
+   <div className="location-description">
+    <p className="daily-location">
+     {weather.name}, {weather.sys.country}
+    </p>
+    <p className="daily-description">{weather.weather[0].description.slice(0, 1).toUpperCase() + weather.weather[0].description.slice(1)}</p>
+   </div>
    <p className="daily-maxmin">
     Min: {weather.main.temp_min.toFixed(1)} °C <br /> Max: {weather.main.temp_max.toFixed(1)} °C
    </p>
